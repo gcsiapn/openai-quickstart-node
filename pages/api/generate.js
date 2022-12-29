042d2eb64818c686b2932eb0869b757787eaa3e0
gcsiapn <58255616+gcsiapn@users.noreply.github.com>
2022-12-29 05:28:23 -0300

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -31,6 +31,10 @@ export default async function (req, res) {
       prompt: generatePrompt(animal),
       temperature: 0.6,
     });
+
+    console.log("Entrada",animal);
+    console.log("Resposta",completion.data.choices[0].text);
+
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
@@ -51,12 +55,15 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
+    const prompt =  
+`Suggest three names for an animal that is a superhero.
 Animal: Cat
 Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
 Animal: Dog
 Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
 Animal: ${capitalizedAnimal}
 Names:`;
+console.log("prompt",prompt);
+return prompt;
+
 }
